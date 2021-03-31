07d7db6e7bc22543db966fecbc0df7a5d405a267
Thammarat-b1992 <thammarat.b1992@gmail.com>
2021-03-31 23:15:44 +0700

    Create Exercise4_Thammarat_P.py

---
+++ Exercise4_Thammarat_P.py
@@ -0,0 +1,9 @@
+FE = 51
+GB = 52
+ICS = 53
+CP = 54
+print("---Your Score---")
+print("Foundation English", "=",FE,"Score")
+print("General Business", "=",GB,"Score")
+print("Introduction to Computer Systems", "=",ICS,"Score")
+print("Computer Programming", "=",CP,"Score")
